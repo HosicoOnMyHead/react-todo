bacc9e7d6bc779a9ea20094a09525376f536678f
HosicoOnMyHead <jinwoo7654@naver.com>
2023-04-11 00:03:04 +0900

    Move another repository

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#5f27cd",
+  textColor: "white",
+  accentColor: "#9c88ff",
+  cardBgColor: "transparent",
+};
